81a01cb4ebbaecadad4d1cef82eca8713bfea7c1
Adarsha Poudel <adarsha@ucsb.edu>
2020-08-19 15:35:51 -0700

    styled it as the format of UC Berkelely course catalog

--- src/components/pages/common/classFormater.js
+++ src/components/pages/common/classFormater.js
@@ -1,94 +1,150 @@
-import React from 'react';
-import { Button, Typography } from '@material-ui/core';
-import DateRangeIcon from '@material-ui/icons/DateRange';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-import ScheduleIcon from '@material-ui/icons/Schedule';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { Button, Typography, Tooltip } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Dropdown from "./dropdown";
+// Icons
+import DateRangeIcon from "@material-ui/icons/DateRange";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import PersonIcon from "@material-ui/icons/Person";
+import ScheduleIcon from "@material-ui/icons/Schedule";
+import StarIcon from "@material-ui/icons/Star";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    paddingTop: "1.5vh",
-    paddingBottom: "1.5vh",
+    paddingTop: "4vh",
+    paddingBottom: "2vh",
   },
   btn: {
     float: "right",
   },
-  firstRow: {
-    marginBottom: "2vh",
+  titleRow: {
+    marginBottom: "1.7rem",
   },
-  secondRow:{
-    marginBottom: "0.75vh",
+  contentRow: {
+    marginBottom: "0.35rem",
+  },
+  openSeatRow: {
+    marginBottom: "0.9rem",
   },
   titleTypography: {
     fontFamily: "Noto Serif, serif",
   },
   iconWrap: {
-    verticalAlign: 'middle',
-    display: 'inline-flex'
+    verticalAlign: "middle",
+    display: "inline-flex",
   },
-  test: {
-    display: "block",
-  },
-  subtitle2: {
-    fontSize: theme.typography.subtitle2.fontSize,
-  }
 }));
 
+const dropdowns = [
+  {
+    dropdownName: "Instructor",
+    defaultValue: "No preference",
+    options: ["Conrad", "No preference", "Mirza", "Adarsha"],
+    fullWidth: false,
+    minWidth: "5rem",
+    fontSize: "0.875rem",
+  },
+];
 
 function CourseFormater(props) {
   const classes = useStyles();
   return (
     <div className={`container-fluid ${classes.root}`}>
-      <div className={`row ${classes.firstRow}`}>
+      <div className={`row ${classes.titleRow}`}>
         <div className="col-8">
-          <Typography variant="h5" className={classes.titleTypography}>{props.courseTitle}</Typography>
+          <Typography variant="h5" className={classes.titleTypography}>
+            {props.courseTitle}
+          </Typography>
         </div>
         <div className="col-4">
-          <Button className="addToScheduleButton" variant="contained" color="secondary" className={classes.btn}>Add To Schedule</Button>
+          <Button
+            className="addToScheduleButton"
+            variant="contained"
+            color="secondary"
+            className={classes.btn}
+          >
+            Add To Schedule
+          </Button>
         </div>
       </div>
-      <div className={`row ${classes.secondRow}`}>
-        <div className="col-3">
-          <Typography variant="h7" className={classes.iconWrap}>
-            <DateRangeIcon />&nbsp;{props.days}
+      <div className={`row ${classes.contentRow}`}>
+        <div className="col-6">
+          <Typography variant="subtitle2" className={classes.iconWrap}>
+            <Tooltip title="Instructors">
+              <PersonIcon color="primary" />
+            </Tooltip>
+            &nbsp;
+            <Dropdown {...dropdowns[0]} />
           </Typography>
         </div>
         <div className="col-3">
-          <Typography variant="h7" className={classes.iconWrap}>
-            <ScheduleIcon />&nbsp;{props.time}
+          <Typography variant="subtitle2" className={classes.iconWrap}>
+            <Tooltip title="Rate My Professor">
+              <StarIcon color="primary" />
+            </Tooltip>
+            &nbsp;{props.rating}
+          </Typography>
+        </div>
+      </div>
+      <div className={`row ${classes.contentRow}`}>
+        <div className="col-3">
+          <Typography variant="subtitle2" className={classes.iconWrap}>
+            <DateRangeIcon color="primary" />
+            &nbsp;{props.days}
           </Typography>
         </div>
         <div className="col-3">
-          <Typography variant="h7" className={classes.iconWrap}>
-            <LocationOnIcon/>&nbsp;{props.location}
+          <Typography variant="subtitle2" className={classes.iconWrap}>
+            <ScheduleIcon color="primary" />
+            &nbsp;{props.time}
+          </Typography>
+        </div>
+        <div className="col-3">
+          <Typography variant="subtitle2" className={classes.iconWrap}>
+            <Tooltip title="Map Locations">
+              <LocationOnIcon color="primary" />
+            </Tooltip>
+            &nbsp;{props.location}
+          </Typography>
+        </div>
+      </div>
+      <div className={`row ${classes.contentRow}`}>
+        <div className="col-3">
+          <Typography variant="subtitle2">
+            <span style={{color: "#767676", fontWeight: "bold"}}>Enroll Code:</span>&nbsp;{props.enrollCode}
+          </Typography>
+        </div>
+        <div className="col-3">
+          <Typography variant="subtitle2">
+            &nbsp;<span style={{color: "#767676", fontWeight: "bold"}}>Units:</span>&nbsp;{props.units}
+          </Typography>
+        </div>
+      </div>
+      <div className={`row ${classes.contentRow} ${classes.openSeatRow}`}>
+        <div className="col-12">
+          <Typography
+            variant="subtitle1"
+            style={{ fontWeight: "bold", marginBottom: "-0.55rem" }}
+          >
+            Open Seats
+          </Typography>
+          <Typography variant="subtitle2">5 Unreserved Seats</Typography>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-11">
+          <Typography variant="subtitle2">
+            <p>{props.description}</p>
           </Typography>
         </div>
       </div>
       <div className="row">
-        <div className="col-3">
-          <Typography variant="subtitle1">Enroll Code:&nbsp;<span className={classes.subtitle2}>{props.enrollCode}</span></Typography>
-        </div>
-        <div className="col-3">
-          <Typography variant="subtitle1">&nbsp;&nbsp;Units:&nbsp;<span className={classes.subtitle2}>{props.units}</span></Typography>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <Typography>Open Seats</Typography>
-        </div>
-        <div className="w-100"></div>
-        <div className="col">
-          <Typography>{props.openSeats}</Typography>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-10">
-          <Typography>{props.description}</Typography>
+        <div className="col-11">
+          <hr className="border border-primary" />
         </div>
       </div>
     </div>
   );
-
 }
 
 export default CourseFormater;

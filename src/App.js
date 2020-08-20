efb8a9b5867c942c1abac52d2ebe16e3f8fe8d45
Adarsha Poudel <adarsha@ucsb.edu>
2020-08-19 20:41:15 -0700

    deleted unwanted component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import PageWrapper from "./components/PageWrapper";
 import Login from "./components/pages/Login";
 import UserInstruciton1 from "./components/pages/UserInstructions/UserInstruction1";
 import UserInstruciton2 from "./components/pages/UserInstructions/UserInstruction2";
-import CourseSearch from "./components/pages/courseSearch";
 
 function App() {
   return (

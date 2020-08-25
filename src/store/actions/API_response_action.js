3f86a06b1beb09ed7378aeab00312b67a26b4432
Adarsha Poudel <adarsha@ucsb.edu>
2020-08-25 01:34:00 -0700

    added more filter field

--- src/store/actions/API_response_action.js
+++ src/store/actions/API_response_action.js
@@ -1,8 +1,8 @@
 import API from "../../utils/API";
 
-export const API_CALL = (quarter, courseName) => {
+export const API_CALL = (quarter, courseName, level, college, area) => {
   return (dispatch) => {
-    API.getClasses(quarter, courseName, 3500, (res) => {
+    API.getClasses(quarter, courseName, level, college, area, (res) => {
       dispatch({
         type: "SEARCH_CLICKED",
         payload: res.data,
